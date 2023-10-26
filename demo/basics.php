27b3cd92ec75d3454f8281b30beddccf7533a494
ngyvc <106571213+ngyvc@users.noreply.github.com>
2023-10-26 13:17:45 -0400

    update error_reporting

--- demo/basics.php
+++ demo/basics.php
@@ -10,7 +10,7 @@ operators</p>
 <?php
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+    // error_reporting(E_ALL);
 
     $firstName = "peter";
     // echo "First Name: $firstName";
