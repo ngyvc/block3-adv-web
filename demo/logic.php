9bea3e49fccd0ff9d0f77487ebdf7d4793490633
Peter Ng <ngy@vaniercollege.qc.ca>
2023-10-26 15:59:23 -0400

    rename generic object and add logic

---
+++ demo/logic.php
@@ -0,0 +1,17 @@
+<?php
+
+$isActive = false;
+
+if($isActive) {
+    echo "is active";
+} else {
+    echo "is not active";
+}
+
+$isActive = !$isActive;
+
+if($isActive) {
+    echo "is active";
+} else {
+    echo "is not active";
+}
