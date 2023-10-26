80ddc6ce102aa23daaa78b3c0c2b672ce58c5a90
Peter Ng <ngy@vaniercollege.qc.ca>
2023-10-26 14:46:33 -0400

    add example

---
+++ demo/example.php
@@ -0,0 +1,26 @@
+<?php
+
+class ObjectName {
+    private $property1;
+    private $property2 = 0;
+
+    public function __construct() {
+        $this->property1 = "not set";
+    }
+    private function getProperty1() {
+        return $this->property1;
+    }
+    private function setProperty1($newValue) {
+        $this->property1 = $newValue;
+    }
+    public function ask() {
+        return "property1: " . $this->getProperty1() . " property2: " . $this->property2;
+    }
+    public function action($data) {
+        $this->setProperty1($data);
+    }
+}
+
+$testObject1 = new ObjectName();
+// $testObject1->action(123);
+echo $testObject1->ask();
