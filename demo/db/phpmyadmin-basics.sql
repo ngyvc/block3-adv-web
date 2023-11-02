9e8ba91a8794cf70d00a108095581da9e9c58b28
Peter Ng <ngy@vaniercollege.qc.ca>
2023-11-02 18:08:09 -0400

    add sql sample and normal forms markdown file

---
+++ demo/db/phpmyadmin-basics.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `ngy_db`.`adv_web` (`ID` INT NOT NULL AUTO_INCREMENT , `username` VARCHAR(256) NOT NULL , `email` VARCHAR(256) NOT NULL , `password` VARCHAR(256) NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB;
+INSERT INTO `adv_web` (`ID`, `username`, `email`, `password`) VALUES (NULL, 'peter', 'peter@test.com', 'hawaii123');
+INSERT INTO `adv_web` (`ID`, `username`, `email`, `password`) VALUES (NULL, 'Rosalie Craig', 'test@test.com', 'hawaii123');
+INSERT INTO `adv_web` (`ID`, `username`, `email`, `password`) VALUES (NULL, 'Elnora Lowe', 'test@test.com', 'hawaii123');
+INSERT INTO `adv_web` (`ID`, `username`, `email`, `password`) VALUES (NULL, 'Maurice Chambers', 'test@test.com', 'hawaii123');
+INSERT INTO `adv_web` (`ID`, `username`, `email`, `password`) VALUES (NULL, 'Manuel Wood', 'test@test.com', 'hawaii123');
+INSERT INTO `adv_web` (`ID`, `username`, `email`, `password`) VALUES (NULL, 'Ralph Henderson', 'test@test.com', 'hawaii123');
+INSERT INTO `adv_web` (`ID`, `username`, `email`, `password`) VALUES (NULL, 'Dorothy McCormick', 'test@test.com', 'hawaii123');
+INSERT INTO `adv_web` (`ID`, `username`, `email`, `password`) VALUES (NULL, 'Ronnie Garcia', 'test@test.com', 'hawaii123');
+INSERT INTO `adv_web` (`ID`, `username`, `email`, `password`) VALUES (NULL, 'Joel Dixon', 'test@test.com', 'hawaii123');
+INSERT INTO `adv_web` (`ID`, `username`, `email`, `password`) VALUES (NULL, 'Seth Gill', 'test@test.com', 'hawaii123');
---
+++ demo/normal-forms.md
@@ -0,0 +1,10 @@
+| Syntax    | Description |
+| --------- | ----------- |
+| Header    | Title       |
+| Paragraph | Text        |
+
+## Employees Table
+
+| employeeID | name  | dept          |
+| ---------- | ----- | ------------- |
+| 1          | Peter | pumpkin eater |
