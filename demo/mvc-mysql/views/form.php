1c14f40c9bdb93031fc1b06f015cc7f65d7b1966
Peter Ng <ngy@vaniercollege.qc.ca>
2023-12-07 16:25:20 -0500

    update mvc demo with insertion into database

---
+++ demo/mvc-mysql/views/form.php
@@ -0,0 +1,7 @@
+<form method="POST">
+    <input type="text" name="name" placeholder="Name" value="<?php echo $_POST['name']?$_POST['name']:""; ?>">
+    <input type="text" name="email" placeholder="Email">
+    <input type="text" name="address" placeholder="Address">
+    <input type="submit" name="submit" value="Submit">
+    <input type="reset" name="reset" value="Reset">
+</form>
