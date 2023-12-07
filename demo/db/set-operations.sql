91c29dac99b59e2bfc5680e8fb1fed2983360668
Peter Ng <ngy@vaniercollege.qc.ca>
2023-12-07 13:52:59 -0500

    add file for set-operations

---
+++ demo/db/set-operations.sql
@@ -0,0 +1,2 @@
+CREATE TABLE `ngy_adv_web`.`Orders` (`ID` INT NOT NULL AUTO_INCREMENT , `Quantity` INT NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB;
+INSERT INTO `Orders` (`ID`, `Quantity`) VALUES (NULL, '1'), (NULL, '15'), (NULL, '25'), (NULL, '35') , (NULL, '55'), (NULL, '75'), (NULL, '85'), (NULL, '115'), (NULL, '125'), (NULL, '325');
